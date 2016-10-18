2e0a85e5788e4fe2c73e3b425c22b39cee2a2ed6
TommyForsberg <tommy.forsberg@hotmail.com>
2016-10-18 14:13:20 +0200

    First commit

---
+++ Biljettbokning/Biljettbokning/Concert.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Biljettbokning
+{
+    public class Concert : Event
+    {
+    }
+}
---
+++ Biljettbokning/Biljettbokning/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Biljettbokning
+{
+    public class Event
+
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfEvent { get; set; }
+    }
+}
